f88eb9d2dd3627315fef860c7eb69438a2008e0e
britinimalloy <britini.malloy@outlook.com>
2019-10-02 19:20:11 -0700

    Add project files.

---
+++ Pyramid/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            String rowCount = Console.ReadLine();
+            int rowNumber;
+            bool good = int.TryParse(rowCount, out rowNumber);
+
+            while (!good) 
+            { 
+                Console.Write("Enter only whole numbers.\n");
+                rowCount = Console.ReadLine();
+                good = int.TryParse(rowCount, out rowNumber);
+
+
+            }
+         
+                    for(int row = 0; row < rowNumber; row++)
+            {
+
+                for (int column = rowNumber; column > row; column--)
+                    Console.Write(" ");
+
+                for (int column = 0; column < (2 * row + 1); column++)
+                    Console.Write("*");
+                Console.WriteLine();
+           
+            }
+            Console.ReadKey();
+        }
+    }
+}
